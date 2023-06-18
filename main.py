b117f7c78321992a951afcf7e8aba6eca36912fd
Sebin Jung <sebinrain@gmail.com>
2023-06-18 21:01:32 +0900

    다국어 번역기: Backend

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
+### main.py 다국어 번역기 백엔드 코드
+
+from flask import Flask, request, render_template
 import openai
 
 # OpenAI API 키 설정
-openai.api_key = 'KEY'
+openai.api_key = "KEY"
 
-# ChatGPT에 대한 대화 시작
-def chat_with_gpt(prompt):
+# Flask application 생성
+app = Flask(__name__) 
+
+@app.route("/translater", methods=["post"])
+def translater():
+    # 주고받는 데이터 형식을 json으로 지정
+    data = request.json # 파싱된 json 데이터(request 객체에는 요청한 데이터를 파싱한 값이 들어있음)
+    
+    language = data["language"] # ai가 번역한 글
+    text = data["inputText"] # 사용자가 입력한 text
+
+    prompt = f"{text}\n\nTranslate this sentence into {language}"
+
     response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',  # ChatGPT 엔진 선택(2023년에 새롭게 출시된 모델)
-        # prompt=prompt, (문법이 바뀌어서 아래와 같이 입력해야 함)
+        model='gpt-3.5-turbo',  # ChatGPT 엔진 선택
         messages=[
+            {
+                "role": "system",
+                "content": "You are a translater"
+            },
             {
                 "role": "user",
                 "content": prompt
             }
         ],
-        temperature=0.7,  # 0 ~ 2 사이, 낮은 값은 보수적인 응답, 높은 값은 더 다양한 응답을 생성합니다.
+        temperature=1.5,  # 0 ~ 2 사이(디폴트:1), 낮은 값은 보수적인 응답, 높은 값은 더 다양한 응답을 생성합니다.
         max_tokens=100,  # 모델이 생성하는 최대 토큰 수
-        n=1,  # 생성할 응답 수
+        # n=1,  # 생성할 응답 수(디폴트:1)
         stop=None,  # 응답 중지 시퀀스
         timeout=None  # 요청 시간 초과 (옵션)
     )
-    if response.choices:
-        return response.choices[0].message.content
-    return ''
+    return response["choices"][0]["message"]["content"]
+    # if response.choices:
+    #     return response.choices[0].text.strip()
 
-# ChatGPT와 상호 작용
-while True:
-    user_input = input("사용자: ")
-    prompt = f"사용자: {user_input}\nAI: "
-    output = chat_with_gpt(prompt)
-    print("AI:", output)
+@app.route("/web")
+def web():
+    return render_template("index.html") # templates 아래의 index.html 파일 리턴함
+
+app.run() # host, port 직접 지정도 가능 host="0.0.0.0", port=80
